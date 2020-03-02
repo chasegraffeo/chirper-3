3b2c4178a1dff64e208159a53681df8d925478ca
Chase Graffeo <chasegraffeo@gmail.com>
2020-03-01 18:31:44 -0600

    understanding branchs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+import Spinner from "react-bootstrap/Spinner";
 import ChirpForms from "./ChirpForms";
 import ChirpTimeline from "./ChirpTimeline";
 
